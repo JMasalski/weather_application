462cb5e6f584cad981cc863f9763c44d3c5266c5
Jakub Masalski <105817412+JMasalski@users.noreply.github.com>
2025-04-09 11:54:35 +0200

    Update FutureForecast.jsx

--- src/comonents/FutureForecast.jsx
+++ src/comonents/FutureForecast.jsx
@@ -9,7 +9,12 @@ const FutureForecast = () => {
 
   return (
     <div className="p-2 md:p-4 lg:p-8 mt-2 md:mt-5 rounded-xl bg-primary">
-      <h1 className="text-white text-2xl font-bold">10 Day Forecast</h1>
+      <div className="flex flex-col">
+        <h1 className="text-white text-2xl font-bold">3 Day Forecast</h1>
+          <h3 className="text-sm text-gray-300">
+            It used to show 10 days (thanks to the premium trial), but it's expired now
+          </h3>
+        </div>
       {/* Kontener przewijany w poziomie */}
       <div className="overflow-x-auto mt-2 md:mt-5 w-full pb-4
         [&::-webkit-scrollbar]:w-2
